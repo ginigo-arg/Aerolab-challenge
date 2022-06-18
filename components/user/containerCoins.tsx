9825390b52575a36a7ad8b4aedac9161d3230974
ginigo-arg <gabrielinigo99@gmail.com>
2022-06-18 03:21:34 -0300

    modificando para actualizar valores al realizar peticion

--- components/user/containerCoins.tsx
+++ components/user/containerCoins.tsx
@@ -1,11 +1,15 @@
+import { useEffect } from "react";
+import { useFilters } from "../../context/filterContext";
 import SvgCoin from "../icons/coin";
 
 export default function ContainerCoins(): JSX.Element {
+  const { state } = useFilters();
+  useEffect(() => {}, [state]);
   return (
     <>
       <div className="w-26 border rounded-md flex flex-row justify-between items-center px-2 py-1 ">
         <SvgCoin />
-        <p className="font-bold">32.212</p>
+        <p className="font-bold">{state.user.points}</p>
       </div>
     </>
   );
