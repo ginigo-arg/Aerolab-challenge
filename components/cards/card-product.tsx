170893a966c792fc18f897ba8fbfede0fe906519
ginigo-arg <gabrielinigo99@gmail.com>
2022-06-18 21:05:36 -0300

    call peticion reedem on single product

--- components/cards/card-product.tsx
+++ components/cards/card-product.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { useFilters } from "../../context/filterContext";
+import { postReedem } from "../../pages/api/api";
 import SvgCoin from "../icons/coin";
 
 type CardProduct = {
@@ -5,6 +8,7 @@ type CardProduct = {
   category: string;
   price: number;
   image: string;
+  id: string;
 };
 
 export default function CardProduct({
@@ -12,7 +16,20 @@ export default function CardProduct({
   category,
   price,
   image,
+  id,
 }: CardProduct) {
+  const [loading, setLoading] = useState<boolean>(false);
+  const { handleRestart } = useFilters();
+  const reedemProduct = async () => {
+    setLoading(true);
+    const resp = await postReedem(id);
+    if (resp) {
+      setLoading(false);
+      handleRestart(price);
+    }
+  };
+  useEffect(() => {});
+
   return (
     <>
       <div className="flex justify-center">
@@ -27,10 +44,14 @@ export default function CardProduct({
             <h5 className="text-gray-900 text-md font-medium mb-1">{name}</h5>
             <p className="text-gray-700 text-base mb-4">{category}</p>
             <button
+              onClick={reedemProduct}
+              disabled={loading}
               type="button"
-              className=" w-full h-10 font-bold tracking-wide bg-gradient-to-r from-sky-500 to-indigo-500 inline-block px-6 py-2.5  text-white text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg"
+              className={`${
+                loading ? "cursor-not-allowed" : "cursor-pointer"
+              } w-full h-10 font-bold tracking-wide bg-gradient-to-r from-sky-500 to-indigo-500 inline-block px-6 py-2.5  text-white text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg`}
             >
-              Reedem
+              {loading ? "Processing..." : "Reedem"}
             </button>
           </div>
         </div>
