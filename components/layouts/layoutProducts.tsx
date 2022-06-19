ce81e4f7a92e6d922d8e4cccced6447b4d809525
ginigo-arg <gabrielinigo99@gmail.com>
2022-06-19 20:23:11 -0300

    fixed auto scroon en filtercategory

--- components/layouts/layoutProducts.tsx
+++ components/layouts/layoutProducts.tsx
@@ -3,5 +3,5 @@ type Props = {
 };
 
 export default function LayoutProducts({ children }: Props) {
-  return <div className="mx-auto max-w-6xl">{children}</div>;
+  return <div className="mx-auto max-w-6xl min-h-screen">{children}</div>;
 }
