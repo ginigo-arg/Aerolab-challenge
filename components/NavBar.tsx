f7a763edf56423c2350f4fdf16b780ad78158409
ginigo-arg <gabrielinigo99@gmail.com>
2022-06-21 19:03:32 -0300

    fixed responsive

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,10 +2,7 @@ import Image from "next/image";
 import ContainerCoins from "./user/containerCoins";
 import UserDates from "./user/userDates";
 
-type User = {
-  user: string;
-};
-export default function NavBar({ user }: User) {
+export default function NavBar() {
   return (
     <div className="flex flex-row justify-between items-center px-2 lg:px-5 py-2 h-18">
       <div className="w-24 md:w-28">
--- components/layouts/layoutProducts.tsx
+++ components/layouts/layoutProducts.tsx
@@ -3,5 +3,17 @@ type Props = {
 };
 
 export default function LayoutProducts({ children }: Props) {
-  return <div className="mx-auto max-w-6xl min-h-screen">{children}</div>;
+  return (
+    <div className="mx-auto max-w-6xl min-h-screen">
+      <div className="flex flex-row justify-center lg:justify-start space-x-2 mt-10 mb-3">
+        <h5 className="text-center lg:text-start text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sky-400 to-indigo-500">
+          TECH
+        </h5>
+        <h5 className="text-center lg:text-start text-3xl font-extrabold text-gray-800">
+          PRODUCTS
+        </h5>
+      </div>
+      {children}
+    </div>
+  );
 }
